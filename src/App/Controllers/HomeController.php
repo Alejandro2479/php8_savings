8df13a44b5f73747be5911750447cc4534d84a98
Alejandro Otalvaro <65875344+Alejandro2479@users.noreply.github.com>
2024-01-31 10:40:02 -0500

    Update

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use Framework\TemplateEngine;
+
 class HomeController
 {
+    private TemplateEngine $view;
+
+    public function __construct(TemplateEngine $view)
+    {
+        $this->view = new TemplateEngine();
+    }
+
     public function home()
     {
         echo "Hello from the Home Page";
